7ca8dac68ad151f167a8bf051d4240adf498741b
bensmith <benhsmith13@gmail.com>
2015-12-09 12:36:51 -0700

    Sped from video

--- client/js/components/main/comment.spec.js
+++ client/js/components/main/comment.spec.js
@@ -4,69 +4,84 @@ import React        from 'react';
 import ReactDOM     from 'react-dom';
 import TestUtils    from 'react/lib/ReactTestUtils';
 import Comment      from './comment';
-import CommentStore from '../../stores/comment';
+import CommentStore from "../../stores/comment";
 
-fdescribe('Comment', ()=> {
-  var result, props, buttons;
+
+describe('comment', ()=>{
+  var result, props, button;
 
   beforeEach(()=>{
     props = {
       comments: ["MyFirstComment", "BirdsAreCool", "SaladSucks"]
     };
-
-    result = TestUtils.renderIntoDocument(<Comment {...props}/>);
-    buttons = TestUtils.scryRenderedDOMComponentsWithTag(result, 'button');
+    result = TestUtils.renderIntoDocument(<Comment {...props} />);
+    button = TestUtils.scryRenderedDOMComponentsWithTag(result, 'button');
   });
 
-  it('it renders', ()=> {
+  it('Displays comment form', ()=>{
     expect(ReactDOM.findDOMNode(result).textContent).toContain('Comments');
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(result, 'button').length).toEqual(2);
+    expect(button.length).toEqual(2);
     expect(TestUtils.findRenderedDOMComponentWithTag(result, 'input')).toBeDefined();
     expect(TestUtils.findRenderedDOMComponentWithClass(result, 'comment-header')).toBeDefined();
   });
 
-  it('hides the Comments', ()=>{
+  it('hides the comment', ()=>{
     var comments = TestUtils.findRenderedDOMComponentWithClass(result, 'comments');
-    expect(buttons[0].textContent).toContain('Hide');
+    expect(button[0].textContent).toContain('Hide');
     expect(comments.style.display).toContain('block');
-    TestUtils.Simulate.click(buttons[0]);
-    expect(buttons[0].textContent).toContain('Show');
+    TestUtils.Simulate.click(button[0]);
+    expect(button[0].textContent).toContain('Show');
     expect(comments.style.display).toContain('none');
   });
 
-  it('shows the comments', ()=>{
+  it('show the comments', ()=>{
     result.setState({display: false});
     var comments = TestUtils.findRenderedDOMComponentWithClass(result, 'comments');
-    expect(buttons[0].textContent).toContain('Show');
+    expect(button[0].textContent).toContain('Show');
     expect(comments.style.display).toContain('none');
-    TestUtils.Simulate.click(buttons[0]);
-    expect(buttons[0].textContent).toContain('Hide');
+    TestUtils.Simulate.click(button[0]);
+    expect(button[0].textContent).toContain('Hide');
     expect(comments.style.display).toContain('block');
   });
 
-  it('displays comments', ()=>{
+  it('displays comment props', ()=>{
     expect(ReactDOM.findDOMNode(result).textContent).toContain("MyFirstComment");
     expect(ReactDOM.findDOMNode(result).textContent).toContain("BirdsAreCool");
     expect(ReactDOM.findDOMNode(result).textContent).toContain("SaladSucks");
   });
 
-  it('saves the comments using spies', ()=>{
+  it('saves the reply, using spies', ()=>{
     spyOn(result, 'reply');
-    TestUtils.Simulate.click(buttons[1]);
+    TestUtils.Simulate.click(button[1]);
     expect(result.reply).toHaveBeenCalled();
   });
 
-  it('saves the comments checking store', ()=>{
-    ReactDOM.findDOMNode(TestUtils.findRenderedDOMComponentWithTag(result, 'input')).value = 'TestsAreNeat!';
-    TestUtils.Simulate.click(buttons[1]);
-    expect(CommentStore.getComments()).toContain('TestsAreNeat!');
+  it('Saves the reply, checking the store', ()=>{
+    ReactDOM.findDOMNode(TestUtils.findRenderedDOMComponentWithTag(result, 'input')).value = "TestsAreNeat!";
+    TestUtils.Simulate.click(button[1]);
+    expect(CommentStore.getComments()).toContain("TestsAreNeat!");
   });
 
-  it('saves the comments checking the component itself', ()=>{
-    ReactDOM.findDOMNode(TestUtils.findRenderedDOMComponentWithTag(result, 'input')).value = 'TestsAreNeat!';
-    TestUtils.Simulate.click(buttons[1]);
+  it('Saves the reply, by the the component output', ()=>{
+    ReactDOM.findDOMNode(TestUtils.findRenderedDOMComponentWithTag(result, 'input')).value = "AnotherTest";
+    TestUtils.Simulate.click(button[1]);
     props.comments = CommentStore.getComments();
-    result = TestUtils.renderIntoDocument(<Comment {...props}/>);
-    expect(ReactDOM.findDOMNode(result).textContent).toContain('TestsAreNeat!');
+    result = TestUtils.renderIntoDocument(<Comment {...props} />);
+    expect(ReactDOM.findDOMNode(result).textContent).toContain("AnotherTest");
   });
+
+  describe('on the homeP Page', ()=>{
+    beforeEach(()=>{
+      props = {
+        comments: ["MyFirstComment", "BirdsAreCool", "SaladSucks"],
+        home: true
+      };
+      result = TestUtils.renderIntoDocument(<Comment {...props} />);
+      button = TestUtils.scryRenderedDOMComponentsWithTag(result, 'button');
+    });
+    it('does something different', ()=>{
+
+    });
+  })
+
 });
